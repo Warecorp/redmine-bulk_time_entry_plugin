51576750425276f0ce9f28cf72e34371993b59c6
Sergey Yelenski <sergey.yelenski@warecorp.com>
2021-03-06 13:14:19 +0300

    plugin changed

--- init.rb
+++ init.rb
@@ -13,9 +13,8 @@ Redmine::Plugin.register 'redmine-bulk_time_entry_plugin' do
 end
 
 # Patches to the Redmine core.
-require 'dispatcher'
+# require 'dispatcher'
 
-# ActionDispatch::Callbacks.to_param do
 ActiveSupport::Reloader.to_prepare do
   require_dependency 'time_entry'
   TimeEntry.send(:include, BulkTimeEntryPlugin::Patches::TimeEntryPatch)
